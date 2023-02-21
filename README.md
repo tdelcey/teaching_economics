45e6e4437e2c75bd03c238dab75db439bb295ac3
tdelcey <thomdelcey@hotmail.fr>
2023-02-21 19:51:47 +0100

    update monnaie finance

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
 # teaching_economics
 
-
-Ce projet mets en commun des ressources visuelles (graphiques, tableaux) pour appuyer des enseignements en économie. Chaque enseignement comprends trois sous-dossiers: les données, le script utilisé et la visualisation en output.
+Ce projet met en commun des ressources visuelles (graphiques, tableaux) pour appuyer des enseignements en économie. Chaque enseignement comprends trois sous-dossiers: les données, le script utilisé et la visualisation en output. 
---
+++ paths_and_packages.R
@@ -0,0 +1,34 @@
+package_list <- c("here", # building clearer paths
+                  "tidyverse",
+                  "reshape2",
+                  "tidytext",
+                  "ggplot2", 
+                  "readxl")
+
+
+for (p in package_list) {
+  if (p %in% installed.packages() == FALSE) {
+    install.packages(p, dependencies = TRUE)
+  }
+  library(p, character.only = TRUE)
+}
+
+
+# path 
+
+if(str_detect(getwd(), "thomd")){
+  project_path <- "C:/Users/thomd/Documents/MEGA/github/teaching_economics"
+} else {
+  if(str_detect(getwd(), "th5858de")) {
+    project_path <- "C:/Users/th5858de/Desktop/MEGA/github/teaching_economics"
+  } else {
+    project_path <- "path_"
+  }
+}
+
+
+
+course_path <- here(project_path, "monnaie_et_finance")
+data_path <- here(course_path, "data")
+figure_path <- here(course_path, "figure")
+
